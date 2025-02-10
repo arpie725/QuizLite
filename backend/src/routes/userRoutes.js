cbb97dff0841f0c949ec09a22207484d85e6c73c
Ryan Phung <rphung@usc.edu>
2025-02-10 12:47:49 -0800

    feat: add endpoint to retrieve all tags for a user

--- backend/src/routes/userRoutes.js
+++ backend/src/routes/userRoutes.js
@@ -13,11 +13,10 @@ const router = express.Router();
 
 // NOTE: middleware authenticates the token before reaching this endpoint!
 
-/*
-  retrieves all the sets belonging to the user
-    - query the database to find all the sets
-    - return the user and the sets
-*/
+/** retrieves all the sets belonging to the user
+ *  - query the database to find all the sets
+ *  - return user and sets
+ */
 router.get('/sets', async (req, res) => {
   const userId_ = req.userId;
   // interact with the database
@@ -45,6 +44,37 @@ router.get('/sets', async (req, res) => {
           setCount: sets.length,
         },
         sets: setsWithoutUserId,
+      },
+    });
+  } catch (er) {
+    console.log(er);
+    return res
+      .status(500)
+      .json({ success: false, message: 'Internal server error' });
+  }
+});
+
+/** retrieves all the tags belonging to the user
+ *  - query the database to find all tags
+ *  - return user and tags
+ */
+router.get('/tags', async (req, res) => {
+  const userId = req.userId;
+  // interact with the database
+  try {
+    const tags = await prisma.tag.findMany({
+      where: {
+        userId,
+      },
+    });
+    // remove the userId from each tag
+    const tagsWithoutUserId = tags.map(({ userId: _, ...rest }) => rest);
+    // return the tags
+    return res.status(201).json({
+      success: true,
+      message: 'Retrieved all the tags from user',
+      data: {
+        tags: { tagsWithoutUserId },
       },
     });
   } catch (er) {
